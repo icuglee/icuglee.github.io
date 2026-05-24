4aa163afadf7fddd1b4dca9a7b2a4feaa6e7c5d9
Mirai Kiya <futuredayo3021@gmail.com>
2026-05-24 20:25:29 +0900

    test

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const parallax_bg = document.querySelector(".parallax_bg");
 window.addEventListener("scroll", () => {
   if (!parallax_bg) return;
   const rect = parallax_bg.parentElement.getBoundingClientRect();
-  const offset = (window.innerHeight / 2 - rect.top) * 0.2;
+  const offset = - (window.innerHeight / 2 - rect.top) * 0.1;
   parallax_bg.style.transform = `translateY(${offset}px)`;
 });
